a9afadadad63f898134d928e911ec46f68afb559
JITUDISHA <dishasethi01.1@gmail.com>
2026-02-15 16:42:08 +0530

    Resolved some UI  conflicts

--- src/app/config/gallery.ts
+++ src/app/config/gallery.ts
@@ -1,6 +1,7 @@
 export const galleryImages = [
-    "https://res.cloudinary.com/dxi7mifgc/image/upload/v1771082363/image_284_fwwnpj.jpg",
-    "https://res.cloudinary.com/dxi7mifgc/image/upload/v1771082363/image_284_fwwnpj.jpg",
-    "https://res.cloudinary.com/dxi7mifgc/image/upload/v1771082363/image_284_fwwnpj.jpg",
-    "https://res.cloudinary.com/dxi7mifgc/image/upload/v1771082363/image_284_fwwnpj.jpg",
+    "https://res.cloudinary.com/dxi7mifgc/image/upload/v1771152437/N6D3wf_2_djwyjs.svg",
+    "https://res.cloudinary.com/dxi7mifgc/image/upload/v1771152437/N6D3wf_2_djwyjs.svg",
+    "https://res.cloudinary.com/dxi7mifgc/image/upload/v1771152437/N6D3wf_2_djwyjs.svg",
+    "https://res.cloudinary.com/dxi7mifgc/image/upload/v1771152437/N6D3wf_2_djwyjs.svg",
+
 ];
