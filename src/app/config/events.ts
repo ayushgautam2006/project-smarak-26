3a1d8225fb8e851ea5eda9e29c48c835f2ec8ab8
Sujal Kumar Agarwal <sujalnitrkl0596@gmail.com>
2026-02-16 14:41:42 +0530

    loading page changes

--- src/app/config/events.ts
+++ src/app/config/events.ts
@@ -1,25 +1,30 @@
 export const events = [
   {
+    id: 1,
     title: "SAHAYATRI",
     description:
       "SAHAYATRI is a machine learning–based hackathon conducted under Smarak – The Civil Fest of NIT Rourkela. Participants will work with real-world accident data to build intelligent models and contribute toward creating safer and smarter road infrastructure by integrating civil engineering and data science. Participation is completely free for NIT Rourkela students, with a total cash prize pool of ₹25,000.",
   },
   {
+    id: 2,
     title: "Gati Sheel",
     description:
       "Gati Sheel is an innovative, team-based competition where participants will design and construct a functional roller coaster track using engineering principles. Teams will be evaluated based on the duration the marble remains in motion on the track and the overall aesthetics and structural creativity of their model. This competition challenges participants to apply concepts of physics, structural stability, and design efficiency, fostering teamwork, problem-solving, and innovation.",
   },
   {
+    id: 3,
     title: "Setu",
     description:
       "Setu is a dynamic team-based competition where participants will design and construct a suspension bridge using specified materials. The structures will be evaluated based on maximum load-bearing capacity and aesthetic appeal. This event challenges teams to apply principles of structural engineering, material efficiency, and design innovation. The team achieving the highest overall score will be declared the winner, demonstrating excellence in both strength and creativity.",
   },
   {
+    id: 4,
     title: "Pramanam",
     description:
       "Pramanam is an engaging team-based quiz competition that tests participants’ fundamental knowledge across diverse subjects, including general awareness, current affairs, science, and history. Teams will compete in multiple challenging rounds that assess analytical thinking, problem-solving skills, and teamwork. The event promotes intellectual curiosity, knowledge retention, and a spirit of healthy competition.",
   },
   {
+    id: 5,
     title: "Shilp Setu",
     description:
       "Shilp Setu is a hands-on team-based competition where participants will design and construct a stable, load-bearing arch bridge using materials like sand, bricks, and water. This challenge tests structural engineering skills, creativity, and material efficiency. Bridges will be evaluated based on strength, load-bearing capacity, and overall design aesthetics. The competition encourages innovation in construction techniques while providing practical exposure to architectural and civil engineering principles.",
