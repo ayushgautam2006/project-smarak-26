6c31ce3d770b26bebfa527156b6106ae7ae385b9
Sujal Kumar Agarwal <sujalnitrkl0596@gmail.com>
2026-02-16 14:06:20 +0530

    minor error fixes

--- src/config/contact.js
+++ src/config/contact.js
@@ -2,16 +2,16 @@ export const footerCards = [
   {
     id: 1,
     title: "find us here",
-    image: "@/images/location.webp",
+    image: "/images/location.webp",
   },
   {
     id: 2,
     title: "team smarak",
-    image: "@/images/team.webp",
+    image: "/images/team.webp",
   },
   {
     id: 3,
     title: "nit rourkela",
-    image: "@/images/nitr.webp",
+    image: "/images/nitr.webp",
   },
 ];
